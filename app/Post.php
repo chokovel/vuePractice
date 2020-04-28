fd97df8e23675c762fe6d6339c47c1242910f2ee
chokovel <sweetelf20@gmail>
2020-04-28 02:10:44 +0100

    initial commit

---
+++ app/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = '9jacrib';
+    protected $fillable = ['title', 'body'];
+}
